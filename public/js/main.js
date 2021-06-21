49960f614ea91ac50ddf2f9632ba184d4b011f13
Junior Carvalho <carvalhojunior133@gmail.com>
2021-06-21 16:14:04 -0300

    projeto concluido

--- public/js/main.js
+++ public/js/main.js
@@ -84,37 +84,9 @@ $("#botao-reiniciar").click(function(){
            }
     });
    }   
-   
-   function inserePlacar(){
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Seu-nome";
-    var numPalavras = $("#contador-palavras").text();
-    var linha = novaLinha(usuario, numPalavras);
-    linha.find(".botao-remover").click(removeLinha);
-    corpoTabela.append(linha);
-   }
-   function novaLinha(usuario,palavras) {
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href","#").addClass("botao-remover");
-    var icone = $("<i>").addClass("small").addClass("materialicons").text("delete");
-    link.append(icone);
-    colunaRemover.append(link);
-
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-    return linha;
-   }    
+       
    function finalizaJogo() {
     campo.attr("disabled", true);
     campo.toggleClass("campo-desativado");
     inserePlacar();
    }
-   function removeLinha(event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
